de4eae2eed3130d46dbc441430cc456123dfe755
Mateus <mateusrochamr133@gmail.com>
2024-04-30 17:25:31 -0300

    Iniciando o Projeto Barbearia com o Professor Bruno do Senac Santos com php e javascript

---
+++ php/cadBarbeiro.php
@@ -0,0 +1,16 @@
+<?php
+
+    #isset - Função que comenta se algo existe, verificando se    
+    if(isset($_POST['nome'])&&isset($_POST['senha'])&&isset($_POST['email'])&&isset($_POST['cpf'])&&isset($_POST['telefone'])&&isset($_POST['aniversario'])&&isset($_POST['data'])) {
+
+    }
+    
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $telefone = $_POST['telefone'];
+    $aniversario = $_POST['aniversario'];
+    $dataInicio = $_POST['data'];
+
+?>
